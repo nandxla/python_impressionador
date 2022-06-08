bb7d751f011151f7c44b2a79f7e295c0a4ff71e9
Fernando Santos <s4antosf3rnando@gmail.com>
2022-06-08 10:19:16 -0300

    mod 15 finalizado

---
+++ 15_modulos_e_OO/main.py
@@ -0,0 +1,11 @@
+"""
+- Tudo para o python é um objeto
+- Como tudo é um objeto, tudo em python tem métodos e atributos
+
+- Módulos são arquivos em python que contem objetos prontos para uso
+"""
+
+import webbrowser as wb
+import time 
+
+wb.open("http://google.com")
